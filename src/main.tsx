7909f13f10250384d3a07ccff281fe5b01ba96d7
khatl.fe <luu.kha@hitachivantara.com>
2023-10-16 15:14:16 +0700

    [update] clear unused file

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,7 @@ import "./index.css";
 
 import { Protected } from "./middleware/Protected";
 import RootLayout from "./routes/RootLayout";
-const Users = lazy(() => import("./routes/Users"));
-const UserDetails = lazy(() => import("./routes/UserDetails"));
 const Home = lazy(() => import("./routes/Home"));
-const Contact = lazy(() => import("./routes/Contact"));
-const About = lazy(() => import("./routes/About"));
 const ErrorPage = lazy(() => import("./routes/ErrorPage"));
 const Auth = lazy(() => import("./routes/auth/AuthenticationForm/AuthenticationForm"));
 
@@ -25,40 +21,16 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
-      {
-        path: "contact",
-        element: <Contact />,
-        children: [
-          {
-            path: "number",
-            element: <h1>Contact me on this no: 09234232342342342344</h1>,
-          },
-          {
-            path: "email",
-            element: <h1>Contact me on this account: contactme@gmail.com</h1>,
-          },
-        ],
-      },
-      {
-        path: "about",
-        element: <Protected><About /></Protected>,
-      },
-      {
-        path: "users",
-        element: (
-          <Protected>
-            <Users />
-          </Protected>
-        ),
-      },
-      {
-        path: "users/:id",
-        element: (
-          <Protected>
-            <UserDetails />
-          </Protected>
-        ),
-      },
+     
+     // if path is need to logged in, wrap it in to <Protected/> component 
+      // {
+      //   path: "users/:id",
+      //   element: (
+      //     <Protected>
+      //       <UserDetails />
+      //     </Protected>
+      //   ),
+      // },
       {
         path: "auth/login",
        element: <Auth />,
